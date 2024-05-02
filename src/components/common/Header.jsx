afefe5939e664144482cf4b825082dfb018fc2f5
Kapilmahar987 <maharkapil9@gmail.com>
2024-05-02 11:23:18 +0530

    khush khush khush

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../stylesheets/Header.css";
-
+const dataCenter = localStorage.getItem("center_data");
 const Header = () => {
   return (
     <>
       <header id="header" className="fixed-top navbar">
-        PM-DAKSH
+        {console.log(dataCenter)}
+        PM-DAKSH {dataCenter}
       </header>
     </>
   );

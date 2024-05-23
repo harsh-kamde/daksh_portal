d08f88734d0161576919310150894bd9f196d299
Hariom Singh Rajput <hariommewada484@gmail.com>
2024-05-23 20:40:06 +0530

    Fixed a minor bug

--- src/components/AttendanceReport.jsx
+++ src/components/AttendanceReport.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../stylesheets/AttendanceReport.css";
 import Header from "../components/common/Header";
 import { Button } from "antd";
 import { useAuth } from "../store/auth";
 
-const dataCenter = JSON.parse(localStorage.getItem("center_data"));
-
 const AttendanceReport = () => {
   // const batch_details = localStorage.getItem("batch");
+  const [dataCenter, setDataCenter] = useState({});
+
+  useEffect(() => {
+    const centerData = JSON.parse(localStorage.getItem("center_data")) || {};
+    setDataCenter(centerData);
+  }, []);
 
   const { studentData, courseData, batchData, actualAttendanceData } =
     useAuth();
@@ -35,9 +39,9 @@ const AttendanceReport = () => {
         <h5 className="name">PM DAKSH</h5>
 
         <div className="report-data">
-          <h5>{dataCenter.center_name}</h5>
-          <h5>{dataCenter.district}</h5>
-          <h5>{dataCenter.state}</h5>
+          <h5>{dataCenter.center_name && dataCenter.center_name}</h5>
+          <h5>{dataCenter.district && dataCenter.district}</h5>
+          <h5>{dataCenter.state && dataCenter.state}</h5>
         </div>
 
         <div className="report-data">
